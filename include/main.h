352efc0ef16500041d8577c18fd36d0699b51bd3
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2024-09-01 15:43:16 -0300

    Added VAO switcher

--- include/main.h
+++ include/main.h
@@ -1,5 +1,16 @@
+#ifndef TDE_MAIN
+#define TDE_MAIN
+
+#include <glad.h>
+
 #define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 700
 
 #define OPGL_MAJOR_VER 3
 #define OPGL_MINOR_VER 3
+
+struct VaoData {
+    GLuint vao; unsigned int vertex_count;
+};
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
 
 #include <main.h>
 
-void finish();
 void key_event_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 bool draw_wireframe = false;
+
+struct VaoData* vaos;
+unsigned int vao_count = 0;
+unsigned int current_vao = 0;
 
 // Print an error with glfw and exit
 void exit_with_error_glfw(const char* error_text) {
@@ -18,19 +22,13 @@ void exit_with_error_glfw(const char* error_text) {
     glfwGetError(&error_description);
 
     fprintf(stderr, "\n[E] ERROR: %s\n%s\n\n", error_text, (char*) error_description);
-    finish();
     exit(EXIT_FAILURE);
 }
 
 // Print a generic error and exit
 void exit_with_error_generic(const char* error_text, const char* error_description) {
     fprintf(stderr, "\n[E] ERROR: %s\n%s\n\n", error_text, error_description);
-    finish();
     exit(EXIT_FAILURE);
-}
-
-void finish() {
-    glfwTerminate();
 }
 
 // Should run when the size of the window is changed
@@ -82,8 +80,18 @@ void handle_input(GLFWwindow* window) {
 
 // Runs at every key press
 void key_event_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    // Switch wireframe mode
     if (key == GLFW_KEY_W && action == GLFW_PRESS) {
         draw_wireframe = !draw_wireframe;
+    }
+
+    // Change VAO
+    if (key == GLFW_KEY_V && action == GLFW_PRESS) {
+        if (current_vao == vao_count-1) {
+            current_vao = 0;
+        } else {
+            current_vao += 1;
+        }
     }
 }
 
@@ -166,17 +174,17 @@ GLuint link_shaders() {
     return shader_program;
 }
 
-int main() {
-    // Initializing
-    GLFWwindow* window;
+void add_vao(struct VaoData* vao) {
+    // Just a dynamic array
+    vaos = realloc(vaos, (vao_count+1)*sizeof(struct VaoData));
+    vao_count += 1;
 
-    initialize(&window);
+    vaos[vao_count-1] = *vao;
+}
 
-    // Build, compile and link the shaders
-    GLuint shader_program = link_shaders();
-
+void vertex_spec() {
     // Defining each vertex
-    double vertices[] = {
+    double vertices1[] = {
         -1.0f, -0.5f, 0.0f, // left  
         -0.0f, -0.5f, 0.0f, // right 
         -0.5f, 0.5f, 0.0f,  // top  
@@ -186,18 +194,18 @@ int main() {
         0.5f, 0.5f, 0.0f  // top  
     };
 
-    GLuint vertex_count = sizeof(vertices) / sizeof(double);
+    GLuint vertex_count1 = (sizeof(vertices1) / sizeof(double)) / 3;
 
     // Setting VBO do draw the defined vertices
     // and VAO to remember what to draw
-    GLuint vao, vbo;
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
+    GLuint vao1, vbo1;
+    glGenVertexArrays(1, &vao1);
+    glGenBuffers(1, &vbo1);
 
-    glBindVertexArray(vao);
+    glBindVertexArray(vao1);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3*sizeof(double), (void*)0);
     glEnableVertexAttribArray(0);
@@ -206,8 +214,82 @@ int main() {
 
     glBindVertexArray(0);
 
+    struct VaoData temp1 = {
+        vao1, vertex_count1
+    };
+
+    struct VaoData *vao_1 = malloc(sizeof(struct VaoData));
+    memcpy(vao_1, &temp1, sizeof(struct VaoData));
+
+    add_vao(vao_1);
+
+    // Doing it again
+
+    // Defining each vertex
+    double vertices2[] = {
+        -1.0f, 0.5f, 0.0f, // left  
+        -0.0f, 0.5f, 0.0f, // right 
+        -0.5f, -0.5f, 0.0f,  // top  
+        
+        1.0f, 0.5f, 0.0f, // left  
+        0.0f, 0.5f, 0.0f, // right 
+        0.5f, -0.5f, 0.0f  // top  
+    };
+
+    GLuint vertex_count2 = (sizeof(vertices1) / sizeof(double)) / 3;
+
+    // Setting VBO do draw the defined vertices
+    // and VAO to remember what to draw
+    GLuint vao2, vbo2;
+    glGenVertexArrays(1, &vao2);
+    glGenBuffers(1, &vbo2);
+
+    glBindVertexArray(vao2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3*sizeof(double), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    struct VaoData temp2 = {
+        vao2, vertex_count2
+    };
+
+    struct VaoData *vao_2 = malloc(sizeof(struct VaoData));
+    memcpy(vao_2, &temp2, sizeof(struct VaoData));
+
+    add_vao(vao_2);
+}
+
+int main() {
+    // Initializing
+    GLFWwindow* window;
+
+    initialize(&window);
+
+    // Build, compile and link the shaders
+    GLuint shader_program = link_shaders();
+
+    // Creating all the VAOs
+    vertex_spec();
+
+    for (int i=0; i <  vao_count; i++) {
+        printf("VAO %d -> ID: %u, Vertex count: %u\n", i, vaos[i].vao, vaos[i].vertex_count);
+    }
+
+    // The VAO to use when drawing
+    struct VaoData vao;
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
+        // Selecting the right VAO to draw
+        vao = vaos[current_vao];
+
         // Input
         handle_input(window);
 
@@ -223,17 +305,15 @@ int main() {
 
         // Drawing with VAO
         glUseProgram(shader_program);
-        glBindVertexArray(vao);
+        glBindVertexArray(vao.vao);
 
         // Finally drawing the vertices
-        glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+        glDrawArrays(GL_TRIANGLES, 0, vao.vertex_count);
 
         // Updating window
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    finish();
 
     return EXIT_SUCCESS;
 }
